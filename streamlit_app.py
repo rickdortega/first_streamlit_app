f20e2e4f5547eedeb6927dccb4daec57a8e0987f
Rick Ortega <rickortega@Ricks-MacBook-Pro.local>
2023-08-28 14:46:47 -0700

    added filtering to df

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect(
+fruits_selected = streamlit.multiselect(
     label="Pick some fruits:", 
     options=list(my_fruit_list.index),
     default=['Avocado', 'Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
